5dc21d5afd99a4344fea576df336d5554f6cb4ce
mugeon <pos04167@kakao.com>
2025-03-15 15:30:58 +0900

    feat : HTTP Interface, DI config

--- moamoa_front/src/main.tsx
+++ moamoa_front/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import "reflect-metadata";
+import React from "react";
+import * as ReactDOM from 'react-dom/client';
+import {QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import queryClient from "./config/QueryClient.tsx";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+);
+
+root.render(
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <App />
+            <ReactQueryDevtools initialIsOpen={true} />
+        </QueryClientProvider>
+    </React.StrictMode>
+);
